1d135657586ecf66e945f695ca75dc2ac707b35d
Utpal Kant <121211189+utpal03@users.noreply.github.com>
2023-05-20 14:35:28 +0530

    Update Fliping_image.java

--- Dsa_questions/Fliping_image.java
+++ Dsa_questions/Fliping_image.java
@@ -8,24 +8,18 @@ public class Fliping_image {
                 { 7, 8, 9 }
         };
         flip(nums);
-        // for (int i = nums.length -1; i >= 0; i--) {
-        //     for (int j = nums[i].length -1; j >= 0; j--) {
-        //         System.out.print(nums[i][j]);
-        //     }
-        //     System.out.println();
-        // }
     }
 
     public static void flip(int[][] arr) {
 
         int [][] result = new int [3][3];
 
-        for (int i = arr.length -1; i >= 0; i--) {
-            for (int j = arr[i].length -1; j >= 0; j--) {
-                result[i][j] = arr[i][j];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                result[i][j] = arr[i][arr[i].length - 1 - j];
             }
         }
-        for (int i = 0; i > result.length;  i++) {
+        for (int i = 0; i < result.length;  i++) {
             for (int j = 0; j < result[0].length; j++) {
                 System.out.print(result[i][j]);
             }
